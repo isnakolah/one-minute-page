9de2951ff250e858be4d2a90d1365057913a6464
Raymond Yegon <raymondyegon17@gmail.com>
2019-03-08 11:39:25 +0300

    Function added to make changes in template pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
-document.getElementsByClassName(
-    
-)
+function User(name, head1, head1text){
+    this.name=name;
+    this.head1=head1
+    this.head1text=head1text;
+}
+
+User.prototype.change= function(changer){
+    var name = this.name 
+    var head1 = this.head1
+    var head1text= this.head1text
+
+    function name(a){
+
+    }
+}
+
+$(function (){
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+$('#myModal').modal('show')
+
+function save(){
+    var data = $('#message-text').val();
+    document.getElementById('head1').innerHTML=data
+}
+
+function save2(){
+    var data = $('#message-text2').val();
+    document.getElementById('head1-text').innerHTML=data
+}
+
+
+
+
+
+
+
+
+
+
+
+
